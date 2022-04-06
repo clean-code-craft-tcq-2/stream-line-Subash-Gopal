8beaf61dc9c88bab7559bf10cd0c6909b1ef9404
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-07 00:32:31 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -6,19 +6,23 @@
 TEST_CASE("Fetch and test the data of soc - Low Limit Test") {
     SocSensor *socSensorData = new SocSensor;
     REQUIRE(socSensorData->fetchSensorData() >= 0);
+    delete socSensorData;
 }
 
 TEST_CASE("Fetch and test the data of soc - High Limit Test") {
     SocSensor *socSensorData = new SocSensor;
     REQUIRE(socSensorData->fetchSensorData() <= 100);
+    delete socSensorData;
 }
 
 TEST_CASE("Fetch and test the data of Temperature - High Limit Test") {
-    SocSensor *socSensorData = new SocSensor;
-    REQUIRE(socSensorData->fetchSensorData() <= -40);
+    TempSensor *tempSensorData = new TempSensor;
+    REQUIRE(tempSensorData->fetchSensorData() <= -40);
+    delete tempSensorData;
 }
 
 TEST_CASE("Fetch and test the data of Temperature - High Limit Test") {
-    SocSensor *socSensorData = new SocSensor;
-    REQUIRE(socSensorData->fetchSensorData() <= 40);
+    TempSensor *tempSensorData = new TempSensor;
+    REQUIRE(tempSensorData->fetchSensorData() <= 40);
+    delete tempSensorData;
 }
