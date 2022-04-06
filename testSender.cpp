0028422ea539b9007c9206c58a4b0bf23de59773
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-07 00:20:31 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -1,4 +1,24 @@
 #define CATCH_CONFIG_MAIN
 
 #include "test/catch.hpp"
-#include "BatteryValidator.h"
+#include "SocSensor.h"
+
+TEST_CASE("Fetch and test the data of soc - Low Limit Test") {
+    SocSensor *socSensorData = new SocSensor;
+    REQUIRE(socSensorData->fetchSensorData >= 0);
+}
+
+TEST_CASE("Fetch and test the data of soc - High Limit Test") {
+    SocSensor *socSensorData = new SocSensor;
+    REQUIRE(socSensorData->fetchSensorData <= 100);
+}
+
+TEST_CASE("Fetch and test the data of Temperature - High Limit Test") {
+    SocSensor *socSensorData = new SocSensor;
+    REQUIRE(socSensorData->fetchSensorData <= -40);
+}
+
+TEST_CASE("Fetch and test the data of Temperature - High Limit Test") {
+    SocSensor *socSensorData = new SocSensor;
+    REQUIRE(socSensorData->fetchSensorData <= 40);
+}
