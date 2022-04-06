4f4beeabc5944fd9d88333f164bcd52a8158f83c
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-07 01:04:55 +0530

    Update Sender.cpp

--- Sender.cpp
+++ Sender.cpp
@@ -10,14 +10,17 @@ private:
     const int streamingLimit = 50;
 
 public:
-    void sendBMSSensorData()
+    bool sendBMSSensorData()
     {
         WriteDataToCSV writeData;
+        int validateCount = 1;
         for (int iterator = 0; iterator < streamingLimit; iterator++)
         {
             BMSParameters *BMSParameters1 = new TempSensor;
             BMSParameters *BMSParameters2 = new SocSensor;
             writeData.printBMSSenderData(iterator, BMSParameters1->fetchSensorData(), BMSParameters2->fetchSensorData());
+            validateCount++;
         }
+        return (validateCount == streamingLimit)?  true :false;
     }
 };
