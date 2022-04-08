7b4c59b0f75f8856525b598482c3b13f610cc075
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-08 15:40:20 +0530

    Update WriteDataToCSV.h

--- WriteDataToCSV.h
+++ WriteDataToCSV.h
@@ -12,20 +12,21 @@ private:
 public:
     WriteDataToCSV()
     {
-        std::cout<<"Opened the CSV file to write the data";
+        std::cout<<"Opened the CSV file to write the data \n";
         BmsSendorDataFile.open("BmsSendorData.csv", std::ios::out | std::ios::trunc);
         BmsSendorDataFile << "Count, SOC Sensor Data, Temperature Sensor Data \n";
     }
 
     ~WriteDataToCSV()
     {
-        std::cout<<"Closed the CSV file";
+        std::cout<<"Closed the CSV file \n";
         BmsSendorDataFile.flush();
         BmsSendorDataFile.close();
     }
 
     void printBMSSenderData(int i, int currentsocValueData, int currentTempValueData)
     {
+        std::cout<< i << "," << currentsocValueData << "," << currentTempValueData << "\n";
         BmsSendorDataFile << i << "," << currentsocValueData << "," << currentTempValueData << "\n";
     }
 };
