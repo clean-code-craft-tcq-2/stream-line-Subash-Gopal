87ea79b31ff83578a7d0c7da379bef2b2e796178
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-14 14:33:24 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -18,29 +18,39 @@ TEST_CASE("Fetches data from a sensor and writtens to CSV FilE") {
     vector<vector<string>> content;
     vector<string> row;
     string line, word;
-    fstream file (fname);
-    if(file.is_open())
-	{
-	    while(getline(file, line))
-	    {
-		row.clear();
-		stringstream str(line);
-		while(getline(str, word, ','))
-		    row.push_back(word);
-		content.push_back(row);
-	    }
-	}
-	else
-	    cout<<"Could not open the file\n";
+    fstream file(fname);
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            row.clear();
+            stringstream str(line);
+            while (getline(str, word, ','))
+                row.push_back(word);
+            content.push_back(row);
+        }
+    }
+    else
+    {
+        REQUIRE(false);
+    }
 
-	for(int i=0;i<content.size();i++)
+   REQUIRE(content.size() == 51);
+    
+    for(int i=1;i<content.size();i++)
+    {
+	for(int j=0;j<content[i].size();j++)
 	{
-	    for(int j=0;j<content[i].size();j++)
-	    {
-		cout<<content[i][j]<<" ";
-	    }
-	    cout<<"\n";
+	    REQUIRE(content[i][0] >= 0);
+	    REQUIRE(content[i][0] <= 49);
+		
+	    REQUIRE(content[i][1] <= 100);
+	    REQUIRE(content[i][1] >= 0);
+		
+            REQUIRE(content[i][2] >= -40);
+	    REQUIRE(content[i][2] <= 40);
 	}
+    }
 }
 
 TEST_CASE("Fetch and test the data of soc - Low Limit Test") {
