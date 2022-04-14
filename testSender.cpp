f8bbafed443ca8dcbd9f5a1c16fe4b6066f10107
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-14 11:27:32 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -15,32 +15,32 @@ TEST_CASE("Fetches data from a sensor and writtens to CSV FilE") {
     REQUIRE(sender.sendBMSSensorData() == true);
     
     string fname = "BmsSendorData.csv";
-	vector<vector<string>> content;
-	vector<string> row;
-	string line, word;
-    	fstream file (fname, ios::in);
-        if(file.is_open())
-        {
-            while(getline(file, line))
-            {
-                row.clear();
-                stringstream str(line);
-                while(getline(str, word, ','))
-                    row.push_back(word);
-                content.push_back(row);
-            }
-        }
-        else
-            cout<<"Could not open the file\n";
+    vector<vector<string>> content;
+    vector<string> row;
+    string line, word;
+    fstream file (fname, ios::in);
+    if(file.is_open())
+	{
+	    while(getline(file, line))
+	    {
+		row.clear();
+		stringstream str(line);
+		while(getline(str, word, ','))
+		    row.push_back(word);
+		content.push_back(row);
+	    }
+	}
+	else
+	    cout<<"Could not open the file\n";
 
-        for(int i=0;i<content.size();i++)
-        {
-            for(int j=0;j<content[i].size();j++)
-            {
-                cout<<content[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
+	for(int i=0;i<51;i++)
+	{
+	    for(int j=0;j<content[i].size();j++)
+	    {
+		cout<<content[i][j]<<" ";
+	    }
+	    cout<<"\n";
+	}
 }
 
 TEST_CASE("Fetch and test the data of soc - Low Limit Test") {
