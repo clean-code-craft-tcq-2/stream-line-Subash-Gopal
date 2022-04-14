b6e5878010dece8160ff2b854585f7959b4f55f6
Subash <91050840+Subash-Gopal@users.noreply.github.com>
2022-04-14 16:17:37 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -62,7 +62,9 @@ TEST_CASE("Fetches data from a sensor and writtens to CSV FilE and tests the val
             std::istringstream(content[i][2]) >> valueTemp;	
             REQUIRE(valueTemp >= -40);
 	    REQUIRE(valueTemp <= 40);
+	    cout<<content[i][j];
 	}
+	cout<<"\n";
     }
 }
 
