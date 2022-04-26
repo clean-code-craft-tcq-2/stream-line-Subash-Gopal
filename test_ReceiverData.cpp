0bff48e76c1ec3d5989b1364912de74326a06189
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-04-26 15:11:22 +0530

    Update test_ReceiverData.cpp

--- test_ReceiverData.cpp
+++ test_ReceiverData.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "computeReceivedData.h"
+#include "Receiver/computeReceivedData.h"
 
 TEST_CASE("To check if file is successfully opened for read data access") 
 {
